f8f7cf8b70fc49ec12301e00bd14cca505244ba4
Shamsiddin Tokhirjonov <84608684+tshamsiddin@users.noreply.github.com>
2021-12-01 18:45:50 +0900

    add sound effect

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var emoji = String.fromCodePoint(128204)
 
 button.addEventListener("click", function () {
     if (input.value.length > 0) {
+        var audio=new Audio("small-bell-ring-01a.mp3");
+        audio.play();
         var li=document.createElement("li");
         li.appendChild(document.createTextNode(`${emoji} ${input.value}`));
         ul.appendChild(li);
@@ -13,6 +15,8 @@ button.addEventListener("click", function () {
 
 input.addEventListener("keypress", function (event) {
     if (input.value.length >0 && event.code === "Enter") {
+        var audio=new Audio("small-bell-ring-01a.mp3");
+        audio.play();
         var li=document.createElement("li");
         li.appendChild(document.createTextNode(`${emoji} ${input.value}`));
         ul.appendChild(li);
